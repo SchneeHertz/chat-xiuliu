558978ea86d7dd6c633f2cc19f41b253340682e3
SchneeHertz <39257008+SchneeHertz@users.noreply.github.com>
2023-09-10 11:55:45 +0800

    add interpreter auto fix error

--- modules/vm.js
+++ modules/vm.js
@@ -12,9 +12,13 @@ const env = {
 const context = new vm.createContext(env)
 
 const javaScriptInterpreterPowerful = ({ code }) => {
-  const script = new vm.Script(code)
-  let result = script.runInContext(context)
-  return JSON.stringify(result)
+  try {
+    const script = new vm.Script(code)
+    let result = script.runInContext(context)
+    return JSON.stringify(result)
+  } catch (error) {
+    return error
+  }
 }
 
 module.exports = {
