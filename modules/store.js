b57c3abb802478274df97ef1ad425aa42b16a233
SchneeHertz <39257008+SchneeHertz@users.noreply.github.com>
2024-05-14 16:57:44 +0800

    update getStore

--- modules/store.js
+++ modules/store.js
@@ -1,5 +1,6 @@
 const path = require('node:path')
 const fs = require('node:fs')
+const { get } = require('lodash')
 
 const { STORE_PATH } = require('../utils/fileTool.js')
 
@@ -13,8 +14,8 @@ try {
   fs.writeFileSync(path.join(STORE_PATH, 'storeData.json'), JSON.stringify(storeData, null, '  '), { encoding: 'utf-8' })
 }
 
-const getStore = (key, defaultValue) => {
-  return JSON.parse(JSON.stringify(storeData[key])) || defaultValue
+const getStore = (key) => {
+  return JSON.parse(JSON.stringify(get(storeData, key, null)))
 }
 
 const setStore = (key, value) => {
