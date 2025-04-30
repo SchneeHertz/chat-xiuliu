15133ab5936cd85743fa1aa2ed94156d4d41f96d
SchneeHertz <39257008+SchneeHertz@users.noreply.github.com>
2025-04-30 14:05:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -342,7 +342,7 @@ const resolveMessages = async ({ resToolCalls, resText, resTextTemp, messages, f
             resTextTemp = ''
           }
           if (STATUS.isAudioPlay) {
-            let speakText = splitResText.join('\n').replace(/[^a-zA-Z0-9一-龟]+[喵嘻捏][^a-zA-Z0-9一-龟]*$/, '喵~')
+            let speakText = splitResText.join('\n')
             speakTextList.push({
               text: speakText,
               speakIndex,
@@ -397,7 +397,7 @@ const resolveMessages = async ({ resToolCalls, resText, resTextTemp, messages, f
 
   if (STATUS.isAudioPlay) {
     if (resTextTemp) {
-      let speakText = resTextTemp.replace(/[^a-zA-Z0-9一-龟]+[喵嘻捏][^a-zA-Z0-9一-龟]*$/, '喵~')
+      let speakText = resTextTemp
       speakTextList.push({
         text: speakText,
         speakIndex,
